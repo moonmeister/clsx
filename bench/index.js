4292b133d34bb3f1967de88182db963ca683f190
Alex Moon <alex.jared.moon@gmail.com>
2021-01-08 13:50:39 -0600

    my bench marks

--- bench/index.js
+++ bench/index.js
@@ -3,14 +3,19 @@ const classnames = require('classnames');
 const classcat = require('classcat');
 const clsx = require('../dist/clsx');
 const old = require('clsx');
+const mine = require('./mine')
 
 function bench(name, ...args) {
 	console.log(`\n# ${name}`);
 	new Suite()
-		.add('classcat*   ', () => classcat.apply(classcat, [args]))
-		.add('classnames  ', () => classnames.apply(classnames, args))
-		.add('clsx (prev) ', () => old.apply(old, args))
-		.add('clsx        ', () => clsx.apply(clsx, args))
+		.add('classcat*      ', () => classcat.apply(classcat, [args]))
+		.add('classnames     ', () => classnames.apply(classnames, args))
+		.add('clsx (prev)    ', () => old.apply(old, args))
+		.add('clsx           ', () => clsx.apply(clsx, args))
+		.add('mine-original  ', () => mine.original.apply(mine.original, args))
+		.add('mine-noJoin    ', () => mine.noJoin.apply(mine.noJoin, args))
+		.add('mine-noReduce  ', () => mine.noReduce.apply(mine.noReduce, args))
+		.add('mine-noObjCheck', () => mine.noObjCheck.apply(mine.noObjCheck, args))
 		.on('cycle', e => console.log('  ' + e.target))
 		.run();
 }
@@ -22,8 +27,8 @@ bench(
 
 bench(
 	'Objects',
-	{ foo:true, bar:true, bax:true, bux:false },
-	{ baz:true, bax:false, bux:true }
+	{ foo: true, bar: true, bax: true, bux: false },
+	{ baz: true, bax: false, bux: true }
 );
 
 bench(
@@ -40,22 +45,22 @@ bench(
 
 bench(
 	'Nested Arrays w/ Objects',
-	['foo', { bar:true, bax:true, bux:false }],
-	['bax', { bax:false, bux:true }]
+	['foo', { bar: true, bax: true, bux: false }],
+	['bax', { bax: false, bux: true }]
 );
 
 bench(
 	'Mixed',
 	'foo', 'bar',
-	{ bax:true, bux:false },
-	['baz', { bax:false, bux:true }]
+	{ bax: true, bux: false },
+	['baz', { bax: false, bux: true }]
 );
 
 bench(
 	'Mixed (Bad Data)',
 	'foo', 'bar',
 	undefined, null, NaN,
-	() => {},
-	{ bax:true, bux:false, 123:true },
-	['baz', { bax:false, bux:true, abc:null }, {}]
+	() => { },
+	{ bax: true, bux: false, 123: true },
+	['baz', { bax: false, bux: true, abc: null }, {}]
 );
---
+++ bench/mine.js
@@ -0,0 +1,102 @@
+//original
+
+exports.original = function classNames(...args) {
+  const classes = args.reduce((acc, arg) => {
+    if (!arg) return acc;
+
+    const argType = typeof arg;
+    if (argType === "string" || argType === 'number') {
+      acc.push(arg);
+    } else if (Array.isArray(arg) && arg.length) {
+      const inner = classNames(...arg);
+      if (inner) acc.push(inner);
+    } else if (argType === "object") {
+      if (arg.toString !== Object.prototype.toString) {
+        classes.push(arg.toString());
+      } else {
+        for (const [key, value] of Object.entries(arg)) {
+          if (value) acc.push(key);
+        }
+      }
+    }
+
+    return acc
+  }, [])
+
+  return classes.join(' ');
+}
+
+function append(acc, arg) {
+  return acc += arg + " "
+}
+
+exports.noJoin = function classNames(...args) {
+  return args.reduce((acc, arg) => {
+    if (!arg) return acc;
+
+    const argType = typeof arg;
+    if (argType === "string" || argType === 'number') {
+      append(acc, arg);
+    } else if (Array.isArray(arg) && arg.length) {
+      const inner = classNames(...arg);
+      if (inner) append(acc, inner);
+    } else if (argType === "object") {
+      if (arg.toString !== Object.prototype.toString) {
+        append(acc, arg).toString()
+      } else {
+        for (const [key, value] of Object.entries(arg)) {
+          if (value) append(acc, key);;
+        }
+      }
+    }
+
+    return acc
+  }, '')
+}
+
+
+exports.noReduce = function classNames(...args) {
+  let classes = ""
+
+  for (const arg of args) {
+    if (!arg) continue;
+    const argType = typeof arg;
+    if (argType === "string" || argType === 'number') {
+      append(classes, arg);
+    } else if (Array.isArray(arg) && arg.length) {
+      const inner = classNames(...arg);
+      if (inner) append(classes, inner);
+    } else if (argType === "object") {
+      if (arg.toString !== Object.prototype.toString) {
+        append(classes, arg.toString());
+      } else {
+        for (const [key, value] of Object.entries(arg)) {
+          if (value) append(classes, key);
+        }
+      }
+    }
+  }
+
+  return classes
+}
+
+exports.noObjCheck = function classNames(...args) {
+  let classes = ""
+
+  for (const arg of args) {
+    if (!arg) continue;
+    const argType = typeof arg;
+    if (argType === "string" || argType === 'number') {
+      append(classes, arg);
+    } else if (Array.isArray(arg) && arg.length) {
+      const inner = classNames(...arg);
+      if (inner) append(classes, inner);
+    } else if (argType === "object") {
+      for (const [key, value] of Object.entries(arg)) {
+        if (value) append(classes, key);
+      }
+    }
+  }
+
+  return classes
+}
